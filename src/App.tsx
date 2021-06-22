1ed7174ef3946b15abb10b8cce87b5f4469f0716
Caio Eduardo Cardoso <caioeduardoit@gmail.com>
2021-06-22 06:00:32 -0300

    Navegação entre páginas e adicionada a página para criar nova sala.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+
+import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Home />
+    <BrowserRouter>
+      <Route path="/" exact component={Home} />
+      <Route path="/rooms/new" component={NewRoom} />
+    </BrowserRouter>
   );
 }
 
